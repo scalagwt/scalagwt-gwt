scalagwt/scalagwt-gwt

commit ec114f5f2a0862e0425750ba885c53d5720cc712

Don't be so silly about the event sink constants in RadioButton Reviewer: jlabanca
git-svn-id: http://google-web-toolkit.googlecode.com/svn/trunk@5553 8db76d5a-ed1c-0410-87a9-c151d255dfc7

user/src/com/google/gwt/user/client/ui/RadioButton.java
@@ -17,10 +17,6 @@ package com.google.gwt.user.client.ui;
 
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.EventTarget;
-import com.google.gwt.event.dom.client.BlurEvent;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.KeyDownEvent;
-import com.google.gwt.event.dom.client.MouseUpEvent;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Event;
@@ -65,10 +61,10 @@ public class RadioButton extends CheckBox {
     super(DOM.createInputRadio(name));
     setStyleName("gwt-RadioButton");
     
-    sinkEvents(Event.getTypeInt(ClickEvent.getType().getName()));
-    sinkEvents(Event.getTypeInt(MouseUpEvent.getType().getName()));
-    sinkEvents(Event.getTypeInt(BlurEvent.getType().getName()));
-    sinkEvents(Event.getTypeInt(KeyDownEvent.getType().getName()));
+    sinkEvents(Event.ONCLICK);
+    sinkEvents(Event.ONMOUSEOUT);
+    sinkEvents(Event.ONBLUR);
+    sinkEvents(Event.ONKEYDOWN);
   }
 
   /**

reference/code-museum/src/com/google/gwt/museum/client/defaultmuseum/DefaultMuseum.java
@@ -65,5 +65,6 @@ public class DefaultMuseum extends Museum implements EntryPoint {
     addIssue(new VisualsForWindowEvents());
     addIssue(new VisualsForDialogBox());
     addIssue(new VisualsForSuggestBox());
+    addIssue(new VisualsForCheckBoxAndRadioButtonEvents());
   }
 }

commit 3a28f9911962138f44d0e43568380cd78adf9083

Fixed a bug introduced in r5553 where RadioButton would sink ONMOUSEOUT instead of ONMOUSEUP.
Patch by: jlabanca
Review by: jgw



git-svn-id: http://google-web-toolkit.googlecode.com/svn/trunk@5627 8db76d5a-ed1c-0410-87a9-c151d255dfc7

user/src/com/google/gwt/user/client/ui/RadioButton.java
@@ -62,7 +62,7 @@ public class RadioButton extends CheckBox {
     setStyleName("gwt-RadioButton");
     
     sinkEvents(Event.ONCLICK);
-    sinkEvents(Event.ONMOUSEOUT);
+    sinkEvents(Event.ONMOUSEUP);
     sinkEvents(Event.ONBLUR);
     sinkEvents(Event.ONKEYDOWN);
   }